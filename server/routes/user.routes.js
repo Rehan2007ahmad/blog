0135067d96367c749ae66d5b431dbd757abe2b88
Rehan Ahmad <rehan1000ahmad@gmail.com>
2026-03-03 13:44:40 +0545

    Reorganize user routes for better structure

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const { createUser,loginUser , getUser, getAllUsers, deleteUser, updateUser, sendOtp, verifyOtp, changePassword} = require('../controllers/user.controller');
 const {authenticateToken} = require('../middleware/auth.middleware')
 
+router.get('/', getAllUsers);
 
 router.post('/register', createUser);
 router.post('/login', loginUser);
-router.get('/', getAllUsers);
-router.get('/:id', getUser);
-router.delete('/:id', deleteUser);
-router.put('/:id', updateUser);
+
 router.post('/send-otp', sendOtp); 
 router.post('/verify-otp', verifyOtp);
 router.post('/change-password', changePassword);
+
+
+router.get('/:id', getUser);
+router.delete('/:id', deleteUser);
+router.put('/:id', updateUser);
 module.exports = router;
